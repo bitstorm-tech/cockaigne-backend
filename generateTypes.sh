5b031965655fbc629b0ca466644ca0f390b821aa
Josef Bauer <josef.bauer.1st@gmail.com>
2023-06-15 01:54:21 +0200

    Correct generateTypes.sh

--- generateTypes.sh
+++ generateTypes.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 echo Generating Typescript types ...
-supabase gen types typescript --local --schema public > $(dirname $0)/../cockaigne-frontend-solidjs/src/lib/supabase/generated-types.ts
+supabase gen types typescript --local --schema public > $(dirname $0)/../cockaigne-frontend/src/lib/supabase/generated-types.ts
 echo DONE
